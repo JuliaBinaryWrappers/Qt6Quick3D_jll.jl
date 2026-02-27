979ea955e8b3e466c8a7aa813b56e1a66b846e17
jlbuild <juliabuildbot@gmail.com>
2026-02-27 07:30:01 +0000

    Qt6Quick3D_jll build 6.10.2+0

--- src/Qt6Quick3D_jll.jl
+++ src/Qt6Quick3D_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Qt6Quick3D")
-JLLWrappers.@generate_main_file("Qt6Quick3D", UUID("6dc365b9-5e99-58d6-8812-efce7277b6ef"))
+JLLWrappers.@generate_main_file("Qt6Quick3D", Base.UUID("6dc365b9-5e99-58d6-8812-efce7277b6ef"))
 end  # module Qt6Quick3D_jll
